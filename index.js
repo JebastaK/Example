4f7519959617a1fcf46a9750ef581cc8732bce70
jebastaR <jebastarakesh@gmail.com>
2024-08-22 11:07:25 +0530

    program

---
+++ index.js
@@ -0,0 +1,16 @@
+var person
+var city="chennai"
+city='banglore'
+
+console.log(typeof(person))
+console.log(city)
+
+const id=101
+console.log(id)
+
+var a,b
+console.log(b)
+let companyName='tcs'
+companyName="cts"
+console.log(companyName)
+
